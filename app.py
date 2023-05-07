694ac8434c55cb88f0fd354f74876b75e784ce66
Hayashka <michtak13@gmail.com>
2023-05-07 09:10:03 +0300

    21

--- app.py
+++ app.py
@@ -28,8 +28,7 @@ from sklearn.metrics import mean_absolute_error
 
 app =  flask.Flask(__name__, template_folder= 'templates')
 
-@app.route('/', methods = ['POST','GET'])
-
+@app.route('/', methods = ['POST', 'GET'])
 @app.route('/index', methods = ['POST', 'GET'])
 def main():
     if flask.request.method == 'GET':
@@ -37,14 +36,25 @@ def main():
     
     if flask.request.method == 'POST':
         with open('model.pkl', 'rb') as f:
-            loaded_model = pickle.load (f)
+            loaded_model = pickle.load(f)
 
-        exp = float(flask.request.form['experience'])
-        y_pred = loaded_model.predict([[exp]])
-        
+        angle = int(flask.request.form['angle'])
+        step = float(flask.request.form['step'])
+        density = float(flask.request.form['density'])
+        elasticity_module = float(flask.request.form['elasticity_module'])
+        hardener_quantity = float(flask.request.form['hardener_quantity'])
+        epoxy_group = float(flask.request.form['epoxy_group'])
+        temperature = float(flask.request.form['temperature'])
+        surface_density = float(flask.request.form['surface_density'])      
+        elasticity_module2 = float(flask.request.form['elasticity_module2'])
+        tensile_strength = float(flask.request.form['tensile_strength'])
+        resin_consumption = float(flask.request.form['resin_consumption'])
+        patch_density = float(flask.request.form['patch_density'])
+
+        y_pred = loaded_model.predict([[angle, step, patch_density, density, elasticity_module, hardener_quantity, epoxy_group, temperature, surface_density, elasticity_module2, tensile_strength, resin_consumption]])
+
         return render_template('main.html', result = y_pred)
-    
-    if __name__=='__main__':
-        app.run()
 
+if __name__ == '__main__':
+    app.run(debug=True)
  
